f7fb246d79b60c74d7559ca848c6674edee61eac
Blitzkreig <crhlstl4nk09@gmail.com>
2024-06-22 19:14:20 -0600

    online web store frontend

---
+++ src/productos.js
@@ -0,0 +1,84 @@
+const productos = [
+    {
+        "id": 1,
+        "name": "Camiseta",
+        "price": 20.99,
+        "isClicked": false,
+        "images": "/products/camiseta-negra.jpeg",
+        "category": "superior",
+        "colors": "white,black",
+        "size": "S,M,L"
+    },
+    {
+        "id": 4,
+        "name": "Chaqueta",
+        "price": 75.99,
+        "isClicked": false,
+        "images": "/products/chaqueta-negra.jpeg",
+        "category": "superior",
+        "colors": "white,black",
+        "size": "S,M,L"
+    },
+    {
+        "id": 5,
+        "name": "Saco Dama",
+        "price": 100.99,
+        "isClicked": false,
+        "images": "/products/saco-blanco.jpeg",
+        "category": "superior",
+        "colors": "white,black",
+        "size": "S,M,L"
+    },
+    {
+        "id": 6,
+        "name": "Saco Caballero",
+        "price": 100.99,
+        "isClicked": false,
+        "images": "/products/saco-blanco-formal.jpeg",
+        "category": "superior",
+        "colors": "white,black",
+        "size": "S,M,L"
+    },
+    {
+        "id": 7,
+        "name": "Vestido",
+        "price": 150.99,
+        "isClicked": false,
+        "images": "/products/vestido-blanco.jpeg",
+        "category": "superior",
+        "colors": "white,black",
+        "size": "S,M,L"
+    },
+    {
+        "id": 2,
+        "name": "Pantalones",
+        "price": 45.50,
+        "isClicked": false,
+        "images": "/products/pantalon-negro.jpeg",
+        "category": "inferior",
+        "colors": "white,black",
+        "size": "S,M,L"
+    },
+    {
+        "id": 8,
+        "name": "Falda",
+        "price": 45.99,
+        "isClicked": false,
+        "images": "/products/falda-cafe.jpeg",
+        "category": "inferior",
+        "colors": "white,black",
+        "size": "S,M,L"
+    },
+    {
+        "id": 3,
+        "name": "Zapatos",
+        "price": 60.00,
+        "isClicked": false,
+        "images": "/products/zapatos-cafe.jpeg",
+        "category": "inferior",
+        "colors": "white,black",
+        "size": "S,M,L"
+    }
+];
+
+export default productos;
